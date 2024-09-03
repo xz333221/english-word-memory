6e7ee394045b4c515750254827348c75f8bfb3ec
徐泽 <569552263@qq.com>
2024-09-04 00:37:23 +0800

    test

--- craco.config.js
+++ craco.config.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 module.exports = {
   webpack: {
-    publicPath: './',
     alias: {
       '@': path.resolve(__dirname, 'src'), // 设置别名
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      // 设置公共路径
+      const publicPath = env === 'production' ? './' : '/';
+      webpackConfig.output.publicPath = publicPath;
+
+      return webpackConfig;
     },
   },
 };
