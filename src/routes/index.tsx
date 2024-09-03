31a65c453f9a5e35073ad3fda5a20214d8004708
徐泽 <569552263@qq.com>
2024-09-04 00:49:44 +0800

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ const AppRoutes: React.FC = () => {
         <Routes>
             {/* 重定向根路径 "/" 到 "/word/Home" */}
             <Route path="/" element={<Navigate to="/word/Home" replace />} />
+            <Route path="/word" element={<Navigate to="/word/Home" replace />} />
             <Route path="/word/Home" element={<Prefix/>}/>
             <Route path="/word/About" element={<About/>}/>
         </Routes>
